19307c19148680ddce203e568ae4193e3179b919
techiedeep07 <pratiksharma1981@gmail.com>
2023-02-26 20:39:05 +0100

    Adding Test Project in Solution

---
+++ GuildedRoseTest/GuildedRoseItemTest.cs
@@ -0,0 +1,23 @@
+﻿using GuildedRose.Model;
+using GuildedRose.Repositories;
+using NUnit.Framework;
+
+namespace GuildedRoseTest
+{
+    [TestFixture]
+    public class GuildedRoseItemTest
+    {
+        [Test]
+        public void Test_NormalItem()
+        {
+            IList<Item> itemLst = new List<Item> { new Item { Name = "Cheddar", SellIn = 15, Quality = 10} };
+            
+            ItemInventory itemInventory = new ItemInventory(itemLst);
+
+            itemInventory.Updation();
+
+            Assert.AreEqual(14, itemLst[0].SellIn);
+            Assert.AreEqual(9, itemLst[0].Quality);
+        }
+    }
+}
